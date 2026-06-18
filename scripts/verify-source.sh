f60aac056d3388595478ce1495572e3d0987b1c4
Edrard A <demonand@gmail.com>
2026-06-18 11:46:35 +0300

    Bump package version to 1.2.1

--- scripts/verify-source.sh
+++ scripts/verify-source.sh
@@ -66,8 +66,8 @@ else
         fail 'Unexpected SOPM package name'
     fi
 
-    if [ "$(xpath_text '/otrs_package/Version' "$SOPM")" = '1.2.0' ]; then
-        pass 'SOPM version is 1.2.0'
+    if [ "$(xpath_text '/otrs_package/Version' "$SOPM")" = '1.2.1' ]; then
+        pass 'SOPM version is 1.2.1'
     else
         fail 'Unexpected SOPM version'
     fi
