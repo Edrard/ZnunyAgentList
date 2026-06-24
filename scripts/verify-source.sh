9d2360656dc3b26a783c140937df8aadb53d6a90
Edrard A <demonand@gmail.com>
2026-06-24 20:17:49 +0300

    Extend safe ticket sync metadata

--- scripts/verify-source.sh
+++ scripts/verify-source.sh
@@ -66,8 +66,8 @@ else
         fail 'Unexpected SOPM package name'
     fi
 
-    if [ "$(xpath_text '/otrs_package/Version' "$SOPM")" = '1.2.6' ]; then
-        pass 'SOPM version is 1.2.6'
+    if [ "$(xpath_text '/otrs_package/Version' "$SOPM")" = '1.2.7' ]; then
+        pass 'SOPM version is 1.2.7'
     else
         fail 'Unexpected SOPM version'
     fi
